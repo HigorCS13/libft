ca5cbc8198f6152915e41ff26192bdcea4c60343
Higor Cunha Silva <hicunha-@c1r8s18.42porto.com>
2023-10-19 10:14:35 +0100

    foi_1

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hicunha- <hicunha-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 00:17:09 by hicunha-          #+#    #+#             */
-/*   Updated: 2023/10/18 13:54:19 by hicunha-         ###   ########.fr       */
+/*   Updated: 2023/10/19 10:14:15 by hicunha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	while (((unsigned char) s1[i] != '\0' || (unsigned char) s2[i] != '\0') 
+			&& i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if ((unsigned char) s1[i] != (unsigned char) s2[i])
+			return ((((char)s1[i]) - ((unsigned char) s2[i])));
 		i++;
 	}
 	return (0);
